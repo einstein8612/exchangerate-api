38b241420294a334e7fb423168474fc0d692fdc5
einstein8612 <128143@student.hetbaken.nl>
2022-06-10 00:20:02 +0200

    helloworld

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("Test")
+import express from "express"
+
+const app = express()
+const port = 80
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
