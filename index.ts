bd2430a93f7bc9aad8bb941364453b39f152cbba
einstein8612 <128143@student.hetbaken.nl>
2022-06-10 00:32:15 +0200

    port change

--- index.ts
+++ index.ts
@@ -1,12 +1,11 @@
 import express from "express"
 
 const app = express()
-const port = 80
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(process.env.PORT || 80, () => {
+  console.log(`Server started on port ${process.env.PORT || 80}`)
 })
