fb5796c6a3b6c505ea0b3e778b5d481417a55287
einstein8612 <128143@student.hetbaken.nl>
2022-06-10 00:51:05 +0200

    api router test process env

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
-import express from "express"
+import express from 'express'
 
 const app = express()
+app.disable('x-powered-by')
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -10,17 +11,17 @@ app.listen(process.env.PORT || 80, () => {
   console.log(`Server started on port ${process.env.PORT || 80}`)
 })
 
-const apiRouter: express.Router  = express.Router()
+const apiRouter: express.Router = express.Router()
 apiRouter.use((req, res, next) => {
-  res.set("Server", "Einstein#0001")
-  res.set("Accept", "application/json")
+  res.set('Accept', 'application/json')
+  res.set('X-Server', 'Einstein#0001')
 
-  res.type("application/json")
+  res.type('application/json')
   next()
 })
 
-apiRouter.get("/", (_, res) => {
-  res.send({"test": "hello"})
+apiRouter.get('/', (_, res) => {
+  res.send({ test: process.env })
 })
 
-app.use("/api", apiRouter)
+app.use('/api', apiRouter)
